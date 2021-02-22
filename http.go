1c492edeeb8300fd3d5276f502dca026f0dbe16c
ekaone <ekaone3033@gmail.com>
2021-02-22 10:09:05 +0700

    fix string

--- http.go
+++ http.go
@@ -1,23 +1,23 @@
 package main
 
 import (
-  “fmt”
-  “log”
-  “net/http”
+	"fmt"
+	"log"
+	"net/http"
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, “Hi, this is Docker Dewiweb!”)
+	fmt.Fprintf(w, "Hi, this is Index page")
 }
 
 func about(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, “Hi, this is Docker Dewiweb!”)
+	fmt.Fprintf(w, "Hi, this is About page")
 }
- 
-func main() {
-  http.HandleFunc(“/”, index)
-  http.HandleFunc(“/about”, about)
 
-  log.Println(“application started at port :8000”)
-  log.Fatal(http.ListenAndServe(“:8000”, nil))
+func main() {
+	http.HandleFunc("/", index)
+	http.HandleFunc("/about", about)
+
+	log.Println("application started at port :8000")
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
